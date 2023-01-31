7dafeb07222447bd3d73738e5896887fe7e39f6c
“Adam <2656237s@student.gla.ac.uk>
2023-01-31 18:29:46 +0000

    Updates as at 31st Jan

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
 from rango import views
 
 app_name = 'rango'
@@ -8,4 +9,5 @@ app_name = 'rango'
 urlpatterns = [
 path('', views.index, name='index'),
 path('about/', views.about, name='about'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+ ]
