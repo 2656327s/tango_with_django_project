17004c9c9aa03d3b40f3a73cc89fcc24b91b30b9
“Adam <2656237s@student.gla.ac.uk>
2023-01-31 19:20:10 +0000

    Updates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -9,10 +9,14 @@ def index(request):
 # Note the key boldmessage matches to {{ boldmessage }} in the template!
 
     category_list = Category.objects.order_by('-likes')[:5]
-
+    pages_list = Page.objects.order_by('-views')[:5] 
+      
     context_dict = {}
+    context_dict = {'categories': category_list, 'pages': pages_list}
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     context_dict['categories'] = category_list
+    context_dict['pages'] = pages_list
+
 
 # Return a rendered response to send to the client.
 # We make use of the shortcut function to make our lives easier.
